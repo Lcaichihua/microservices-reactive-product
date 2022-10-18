5075724a3fc91d03ab3d98a9445239be2676415d
Larry Caichihua Vergara <lcaichihua@gmail.com>
2022-10-18 02:14:40 -0500

    update settings.gradle / add api ,util

--- microservices/product-service/src/main/java/se/magnus/microservices/core/product/ProductServiceApplication.java
+++ microservices/product-service/src/main/java/se/magnus/microservices/core/product/ProductServiceApplication.java
@@ -2,6 +2,7 @@ package se.magnus.microservices.core.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class ProductServiceApplication {
