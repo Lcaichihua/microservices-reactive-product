ef2bf6c19d5e6b500b73f3331a61af667e2d055c
Larry Caichihua Vergara <lcaichihua@gmail.com>
2023-07-03 01:32:05 -0500

    add depdency api and util

---
+++ microservices/review-service/src/main/java/se/magnus/microservices/core/review/services/ReviewServiceImpl.java
@@ -0,0 +1,13 @@
+package se.magnus.microservices.core.review.services;
+
+import se.magnus.api.core.review.Review;
+import se.magnus.api.core.review.ReviewService;
+
+import java.util.List;
+
+public class ReviewServiceImpl implements ReviewService {
+    @Override
+    public List<Review> getReviews(int productId) {
+        return null;
+    }
+}
