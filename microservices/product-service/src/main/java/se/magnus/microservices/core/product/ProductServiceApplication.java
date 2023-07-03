977d1f4d2481c1136bcff912bd68a9490382fd2b
Larry Caichihua Vergara <lcaichihua@gmail.com>
2023-07-02 23:49:24 -0500

    add controller ProductServiceImpl

--- microservices/product-service/src/main/java/se/magnus/microservices/core/product/ProductServiceApplication.java
+++ microservices/product-service/src/main/java/se/magnus/microservices/core/product/ProductServiceApplication.java
@@ -1,8 +1,8 @@
 package se.magnus.microservices.core.product;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class ProductServiceApplication {
---
+++ microservices/product-service/src/main/java/se/magnus/microservices/core/product/services/ProductServiceImpl.java
@@ -0,0 +1,34 @@
+package se.magnus.microservices.core.product.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+import se.magnus.api.core.product.Product;
+import se.magnus.api.core.product.ProductService;
+import se.magnus.api.exceptions.InvalidInputException;
+import se.magnus.util.http.ServiceUtil;
+@RestController
+public class ProductServiceImpl implements ProductService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProductServiceImpl.class);
+    private final ServiceUtil serviceUtil;
+
+    public ProductServiceImpl(ServiceUtil serviceUtil) {
+        this.serviceUtil = serviceUtil;
+    }
+
+
+    @Override
+    public Product getProduct(int productId) {
+
+        LOG.debug("/Product return the found product for product={}",productId);
+
+        if(productId<1){
+            throw new InvalidInputException("Invalid productId"+productId);
+        }
+        if(productId<13){
+            throw new InvalidInputException("No product found "+productId);
+        }
+        return new  Product(productId,"name-"+productId,123,serviceUtil.getServiceAddress());
+    }
+}
